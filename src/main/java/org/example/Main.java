9aae994023f0c5900c2d6098b5a971281a19e2b8
DuckyDuck014 <Dwane.Games.2014@yandex.ru>
2022-10-26 13:44:07 +0700

    Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/controlsystemofschool";
 
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in);
         Connection connection  = DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
         while(true){
             System.out.println("1");
